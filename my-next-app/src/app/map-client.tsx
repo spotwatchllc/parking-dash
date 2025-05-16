9fd81e989ee19f750d14b67cf9d432c67ab47628
Arul Muralikrishna <arulmk@umich.edu>
2025-05-16 19:37:57 -0400

    map appears on web application, but coordinates need fixing

---
+++ my-next-app/src/app/map-client.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const Map = dynamic(() => import('./components/map'), { ssr: false });
+
+export default function MapClient() {
+  return <Map />;
+}
