d2bf046ba8c948c9b2bd659416b4b0bb5175eaff
Arul Muralikrishna <arulmk@umich.edu>
2025-05-20 21:48:46 -0400

    Homepage Barebones and Dashboard

--- my-next-app/src/app/map-client.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const Map = dynamic(() => import('./components/map'), { ssr: false });
-
-export default function MapClient() {
-  return <Map />;
-}
