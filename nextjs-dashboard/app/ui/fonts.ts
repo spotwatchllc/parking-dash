8c4d95357096c048086cd8ce90baacc6bdfed92f
Arul Muralikrishna <arulmk@umich.edu>
2025-05-22 18:04:57 -0400

    improved changes to ui

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,8 +1,14 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Roboto_Mono } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
+});
+
+export const robotoMono = Roboto_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'], // optional: choose which weights you want
+  variable: '--font-roboto-mono', // optional CSS variable
 });
