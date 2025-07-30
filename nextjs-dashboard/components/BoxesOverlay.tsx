fc779ac16c25b43a8bd1ac06d58b92eed168d1ba
jhukkk <janchivv.khulan@gmail.com>
2025-07-30 12:51:27 +0800

    feat: front-end receives data from the backend api and displays it in show boxes page

--- nextjs-dashboard/components/BoxesOverlay.tsx
+++ nextjs-dashboard/components/BoxesOverlay.tsx
@@ -1,2 +1,29 @@
 'use client';
 import useSwr from 'swr';
+import { Box } from '@/lib/boxStore';
+
+const fetcher = ( url: string ) => fetch(url).then(r => r.json());
+
+export function BoxesOverlay() {
+    const { data: boxes, error } = useSwr<Box[]>('/api/boxes', fetcher, { refreshInterval: 1000, });
+
+    if (error) return <p style={{color: 'red'}}>Failed to load boxes.</p>
+    if (!boxes) return <p>Loading bounding boxes...</p>
+
+    return (
+        <svg width={640} height={480} style={{position:'absolute', top:0, left:0}}>
+            {boxes.map(b => (
+                <rect
+                key={b.box_id}
+                x={b.x1}
+                y={b.y1}
+                width={b.x2 - b.x1}
+                height={b.y2 - b.y1}
+                stroke="lime"
+                fill="transparent"
+                strokeWidth={2}
+                />
+            ))}
+        </svg>
+    )
+}
