54bde2c0d1f520e9a28b289e27cdd0410043bd9c
fazenrazen <faizandarsot@gmail.com>
2026-01-20 19:13:33 -0700

    removed the .next builder artifacts

--- nextjs-dashboard/.next/server/app/api/ttn-webhook/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/ttn-webhook/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/[root of the server]__977f39._.js");
-runtime.loadChunk("server/chunks/a7abb_next_8a60a3._.js");
-runtime.loadChunk("server/chunks/_ebfab9._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/ttn-webhook/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.1.6_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/ttn-webhook/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- nextjs-dashboard/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XrQQq/8zVKzAVn533METXT/SxUviWBCuH+a9wgZjiM0=\"\n}"
